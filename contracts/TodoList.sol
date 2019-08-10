7b624ad313c07c0c0c92bd803aaf58cce6eb04f4
Franklin Antony <franklinanton.007@gmail.com>
2019-08-10 20:00:59 +0530

    Added mapping,function and constructor 💫

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,10 +3,20 @@ pragma solidity ^0.5.0;
 contract TodoList {
   uint public taskCount = 0;
 
+  constructor() public {
+        createTask("The first task is to read a lot");
+      }
+
+
   struct Task {
     uint id;
     string content;
     bool completed;
   }
   mapping(uint => Task) public tasks;
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
